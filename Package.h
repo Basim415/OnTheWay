84d4710f16101596ad07bae605fe4455de0f57fd
Basim Shahzad <basim1415@outlook.com>
2024-12-15 14:13:39 -0800

    class created

--- Package.h
+++ Package.h
@@ -4,7 +4,28 @@
 
 #ifndef PACKAGE_H
 #define PACKAGE_H
+#include <string>
+
+using namespace std;
+
+
+class Package {
+private:
+    string carrierName;
+    string reciverName;
+    string destinationBuilding;
+    float weight;
+public:
+    Package(); // Blank constructor
+    Package(string carrierName, string reciverName, string destinationBuilding, float weight);
+
+
+
+};
+
+
+
+
+
 
 #endif //PACKAGE_H
-
-//test
